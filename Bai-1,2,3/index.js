cc5b9499ff3cff88b0be3946b9c6583b32e0890f
ThanhMinh <thanhminh339968az@gmail.com>
2022-09-13 11:17:46 +0700

    đưa nodejs lên heroku

--- Bai-1,2,3/index.js
+++ Bai-1,2,3/index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", "./views");f
 app.listen(3000);
 
 app.get("/", function(req, res){
